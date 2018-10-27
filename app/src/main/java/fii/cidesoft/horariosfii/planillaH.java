0b191ac874da8fa90aa8b18cde3235b2589b3704
EslB <eslevano@hotmail.com>
2018-10-26 20:06:30 -0500

    Initial commit

---
+++ app/src/main/java/fii/cidesoft/horariosfii/planillaH.java
@@ -0,0 +1,13 @@
+package fii.cidesoft.horariosfii;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class planillaH extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planilla_h);
+    }
+}
